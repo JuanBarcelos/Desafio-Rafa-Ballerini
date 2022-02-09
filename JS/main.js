b184afb715883b7c7d2169cce4cb00c49786dee5
JuanBarcelos <juanbarcelos@outlook.com.br>
2022-02-09 07:46:01 -0300

    desafios 1 e 2 concluidos

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+const nome = document.getElementById("name");
+const idade = document.getElementById("idade");
+const result = document.getElementById("result");
+const res = document.getElementById("res");
+
+function calculo() {
+    const nascimento = 2022 - idade.value;
+    if (nome.value != "" && idade.value != "") {
+        if (nascimento <= 0) {
+            return result.innerText = `Caramba ${nome.value}, você e um bichão mermo`;
+        }
+        return result.innerText = `Que legal ${nome.value}, você nasceu em ${nascimento}`;
+    }
+    return result.innerText = `Preencha todos os campos`;
+}
+
+
+
+function troco() {
+    if(idade.value != ""){
+        let moedaDeUmReal = 1 * idade.value;
+        let moedaDeCinquenta = moedaDeUmReal * 2
+        let moedaDeVinteECinco = moedaDeCinquenta * 2
+        let moedaDeDez = moedaDeUmReal * 10
+        let moedaDeCinco = moedaDeVinteECinco * 5
+    
+        res.innerHTML += `<p>Você vai receber ${moedaDeUmReal} moedas de 1 Real de troco</p>`;
+    
+        res.innerHTML += `<p>Você vai receber ${moedaDeCinquenta} moedas de 0,50 Centavos de troco</p>`;
+    
+        res.innerHTML += `<p>Você vai receber ${moedaDeVinteECinco} moedas de 0,25 Centavos de troco</p>`;
+    
+        res.innerHTML += `<p>Você vai receber ${moedaDeDez} moedas de 0,10 Centavos de troco</p>`;
+    
+        res.innerHTML += `<p>Você vai receber ${moedaDeCinco} moedas de 0,05 Centavos de troco</p>`;
+    
+        return res;
+    }
+    res.innerHTML += `<p>Preencha todos os campos</p>`;
+    return res;
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+#Ballerine Semana JavaScript
+Exercicios da semana JavaScript do servidor da @rafaballerini.
+
+##Estes foram os desafios:
+
+![image](https://cdn.discordapp.com/attachments/940273848614518805/940373743870558249/tech_da_semana_-_js.png)
+
+##Meu projeto
+
+![gif](./Desafio JS.gif)
+
+##Discord
+
+https://discord.gg/ballerini
